19846c31efb57f5d1887d462ff85bac2498f1512
Aung Myat Min <koaungmyatmin0@gmail.com>
2025-04-23 03:13:05 +0630

    feat: formatted the files
    sorry this is just a wrong commit

---
+++ src/types.d.ts
@@ -0,0 +1,21 @@
+interface IPlayer {
+	playerId: string;
+	choice: PlayerChoice;
+}
+
+interface IBoard {
+	boardId: string;
+	board: BoardCell[][];
+}
+
+interface IGame {
+	roomId: string;
+	player1: IPlayer;
+	player2: IPlayer;
+	winnerId: string | null;
+	board: IBoard;
+	turn: PlayerChoice;
+	active: boolean;
+}
+
+export { IPlayer, IBoard, IGame };
