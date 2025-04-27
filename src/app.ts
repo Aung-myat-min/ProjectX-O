17911051bffe784ecee9acb2a9f733f5655cba76
Adi <ripedown1@gmail.com>
2025-04-27 09:35:49 +0530

    fixed issues with controllers and added dtos.d.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,16 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 
+// dtos import
+import {
+	CreateRoomData,
+	JoinRoomData,
+	LeaveRoomData,
+	StartGameData,
+	MakeMoveData,
+	LeaveGameData,
+} from "./dtos";
+
 // room event controllers import
 import { createRoom, joinRoom, leaveRoom } from "./controllers/room.controller";
 
@@ -21,17 +31,17 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
 	// room event controllers
-	socket.on("room:create", createRoom);
-	socket.on("room:join", joinRoom);
-	socket.on("room:leave", leaveRoom);
+	socket.on("room:create", (data: CreateRoomData) => createRoom(socket, data));
+	socket.on("room:join", (data: JoinRoomData) => joinRoom(socket, data));
+	socket.on("room:leave", (data: LeaveRoomData) => leaveRoom(socket, data));
 
 	// authenticate user by roomID and userID
 	socket.use(authenticatePlayer);
 
 	// game event controllers
-	socket.on("game:start", startGame);
-	socket.on("game:move", makeMove);
-	socket.on("game:leave", leaveGame);
+	socket.on("game:start", (data: StartGameData) => startGame(socket, data));
+	socket.on("game:move", (data: MakeMoveData) => makeMove(socket, data));
+	socket.on("game:leave", (data: LeaveGameData) => leaveGame(socket, data));
 });
 
 export default server;
--- src/controllers/room.controller.ts
+++ src/controllers/room.controller.ts
@@ -1,5 +1,8 @@
-function createRoom() {}
-function joinRoom() {}
-function leaveRoom() {}
+import { Socket } from "socket.io";
+import { CreateRoomData, JoinRoomData, LeaveRoomData } from "../dtos";
+
+function createRoom(socket: Socket, data: CreateRoomData) {}
+function joinRoom(socket: Socket, data: JoinRoomData) {}
+function leaveRoom(socket: Socket, data: LeaveRoomData) {}
 
 export { createRoom, joinRoom, leaveRoom };
