dd82ae3241e77acebb33198a3b5b379a9135e26d
Adi <ripedown1@gmail.com>
2025-04-29 17:11:31 +0530

    switch case bug fix

--- src/utils/http-response-handler.ts
+++ src/utils/http-response-handler.ts
@@ -10,12 +10,16 @@ export function HttpResponseHandler<T>(data: CustomResponse<T>, res: Response) {
 		switch (data.status) {
 			case CStatus.success:
 				res.status(200).json(data);
+				break;
 			case CStatus.invalidData:
 				res.status(401).json(data);
+				break;
 			case CStatus.fail:
 				res.status(500).json(data);
+				break;
 			case CStatus.notFound:
 				res.status(404).json(data);
+				break;
 			default:
 				res.status(500).json({ message: "Internal Server Error!" });
 		}
