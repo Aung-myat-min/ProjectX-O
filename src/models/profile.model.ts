12961cb57c8fa9c15ea7b17766a0aa19286f5c88
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2026-01-08 01:31:52 +0000

    feat: new model - profile

---
+++ src/models/profile.model.ts
@@ -0,0 +1,22 @@
+class Profile {
+	private _userCode: string;
+	displayName: string;
+	private _createdGame: string[];
+	private _wonMatches: string[];
+	private _currentGame: string | null;
+	createdAt: Date;
+
+	private constructor(userCode: string, displayName: string, createdGame: string[], wonMatches: string[], currentGame: string | null, createdAt: Date) {
+		this._userCode = userCode;
+		this.displayName = displayName;
+		this._createdGame = createdGame;
+		this._wonMatches = wonMatches;
+		this._currentGame = currentGame;
+		this.createdAt = createdAt;
+	}
+
+	public static init(displayName: string){
+		const newUserCode = "";
+		return new Profile(newUserCode, displayName, [], [],  null, new Date());
+	}
+}
